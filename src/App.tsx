eef47e820bd55b77e1eff8e0c3c9617953dcad74
Kaique Bueno <btc.bueno@protonmail.com>
2025-03-17 00:40:40 -0300

    update App.tsx TAWK TO

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,31 @@ const ScrollToTop = () => {
   return null;
 };
 
+declare global {
+  interface Window {
+    Tawk_API: {
+      [key: string]: unknown;
+    };
+  }
+}
+
 function App() {
+  useEffect(() => {
+    const Tawk_API = window.Tawk_API || {};
+    window.Tawk_API = Tawk_API;
+    (function () {
+      const s1 = document.createElement("script");
+      const s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/YOUR_PROPERTY_ID/default";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      if (s0.parentNode) {
+        s0.parentNode.insertBefore(s1, s0);
+      }
+    })();
+  }, []);
+
   return (
     <Router>
       <ScrollToTop />
