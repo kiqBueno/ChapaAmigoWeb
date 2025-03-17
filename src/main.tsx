3fd60e36ae3ede7630a2e2554939cf90f849e8a6
Kaique Bueno <btc.bueno@protonmail.com>
2025-03-17 01:58:11 -0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,15 +10,17 @@ createRoot(document.getElementById("root")!).render(
 );
 
 // Start of Tawk.to Script
-var Tawk_API = Tawk_API || {},
-  Tawk_LoadStart = new Date();
+const Tawk_API: { [key: string]: unknown } = {};
 (function () {
-  var s1 = document.createElement("script"),
+  Object.assign(Tawk_API, Tawk_API || {});
+  const s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
   s1.src = "https://embed.tawk.to/63b7553d47425128790beaa6/1gm229upa";
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
-  s0.parentNode.insertBefore(s1, s0);
+  if (s0.parentNode) {
+    s0.parentNode.insertBefore(s1, s0);
+  }
 })();
 // End of Tawk.to Script
